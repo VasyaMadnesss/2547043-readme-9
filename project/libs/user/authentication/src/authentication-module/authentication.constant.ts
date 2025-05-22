3dde8a30c85a0db8b6afb0b63d31448171f0b54c
Дубровин Василий <thebestmane@outlook.com>
2025-05-23 02:37:07 +0400

    Инициализирует спецификацию в формате openAPI/swagger

--- project/libs/user/authentication/src/authentication-module/authentication.constant.ts
+++ project/libs/user/authentication/src/authentication-module/authentication.constant.ts
@@ -1,3 +1,12 @@
-export const AUTH_USER_EXISTS = 'User with this email exists';
+export const AUTH_USER_EXISTS = 'User with exact email already exists.';
 export const AUTH_USER_NOT_FOUND = 'User not found';
-export const AUTH_USER_PASSWORD_WRONG = 'User password is wrong';
+export const AUTH_USER_PASSWORD_WRONG = 'User password is incorrect';
+
+export const AuthenticationResponseMessage = {
+  LoggedSuccess: 'User has been successfully logged.',
+  LoggedError: 'Password or login is incorrect.',
+  UserFound: 'User found.',
+  UserNotFound: 'User not found.',
+  UserExist: 'User with exact email already exists.',
+  UserCreated: 'User has been successfully created.',
+} as const;
--- project/libs/user/authentication/src/dto/login-user.dto.ts
+++ project/libs/user/authentication/src/dto/login-user.dto.ts
@@ -1,4 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger';
 export class LoginUserDto {
+  @ApiProperty({
+    description: 'Unique user email adress.',
+    example: 'example@email.com'
+  })
   public email!: string;
+
+  @ApiProperty({
+    description: `User's password.`,
+    example: '123abc'
+  })
   public password!: string;
 }
